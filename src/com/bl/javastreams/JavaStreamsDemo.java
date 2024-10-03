0045918c6ad93dcee5da826f729302ade91d0fea
thakursanjeev26 <sanjeevjthakur@gmail.com>
2024-10-03 23:11:54 +0530

    Added:UC2.1_Ability_To_Create_And_Iterate_Stream

---
+++ src/com/bl/javastreams/JavaStreamsDemo.java
@@ -0,0 +1,23 @@
+package com.bl.javastreams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaStreamsDemo {
+    public static void main(String[] args) {
+        System.out.println("Welcome to JavaStreams Api");
+        List<Integer>numbers=new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+        numbers.add(8);
+        numbers.add(9);
+
+        numbers.stream().forEach(num-> System.out.println(num));
+
+    }
+}
